ae13c9690f3632485fe3a4706b047d3d469142b7
Mitchell Werksy <dungeonmasterwersky@gmail.com>
2021-01-28 20:16:42 -0800

    first commit

---
+++ src/main/java/com/mwersky/FinalProject/App.java
@@ -0,0 +1,13 @@
+package com.mwersky.FinalProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
